231b2e396412ce236b2a7d061b7374a82415c42a
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-12 20:01:16 +0530

    Add files via upload

---
+++ Interface/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+
+        interface IShape
+        {
+            void Draw();
+        }
+
+        interface IResizable
+        {
+            void Resize();
+        }
+
+        class ResizableCircle : IShape, IResizable
+        {
+            public void Draw()
+            {
+                Console.WriteLine("Drawing a resizable circle.");
+            }
+
+            public void Resize()
+            {
+                Console.WriteLine("Resizing the circle.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ResizableCircle obj = new ResizableCircle();
+            obj.Draw();
+            obj.Resize();
+            Console.Read();
+        }
+    }
+}
